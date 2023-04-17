d7250934f3d87ec21eda41778a3d4b9c9bcd0ec4
leesemin <leesemin222@gmail.com>
2023-04-17 21:45:10 +0900

    전체 적용

--- example.js
+++ example.js
@@ -2,14 +2,16 @@
 
 // console.log(Math.min(...a))
 
-// const a = [1,2,3,4,5]
-// const b = {
-//   first : 1,
-//   second: 2,
-//   third:3,
-//   fourth:4,
-//   fifth:5,
-// }
+const a = [1,2,3,4,5]
+const b = {
+  first : 1,
+  second: 2,
+  third:3,
+  fourth:4,
+  fifth:5,
+}
+const c = null
 
-// console.log(Array.isArray(a))
-// console.log(Array.isArray(b))
+console.log(Array.isArray(a))
+console.log(Array.isArray(b))
+console.log(Array.isArray(c))
