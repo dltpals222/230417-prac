d989788d63c6002c98bafe1238074cbae4e07011
leesemin <leesemin222@gmail.com>
2023-04-17 21:27:40 +0900

    4월 17일 개인적인 시도

---
+++ example.js
@@ -0,0 +1,15 @@
+// const a = [3,1,4,2]
+
+// console.log(Math.min(...a))
+
+// const a = [1,2,3,4,5]
+// const b = {
+//   first : 1,
+//   second: 2,
+//   third:3,
+//   fourth:4,
+//   fifth:5,
+// }
+
+// console.log(Array.isArray(a))
+// console.log(Array.isArray(b))
